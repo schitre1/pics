879f36698e423938fe772078cd4a4b44d3642362
Chitre <schitre@paypal.com>
2019-03-24 22:02:46 -0700

    create event handler and controlled input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = {term: ''};
+    //when someone changes text inside text box
+    onInputChange(event){
+        //event.target.value will contain text that user typed
+        console.log(event.target.value);
+    }
+
     render(){
         return (
             <div className="ui segment">
                 <form className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text"></input>
+                        <input 
+                            type="text" 
+                            value={this.state.term} 
+                            onChange={(e) => this.setState({term: e.target.value})}
+                        />
                     </div>
                 </form>
                 
@@ -15,5 +26,8 @@ class SearchBar extends React.Component {
         );
     }
 }
+//we leave the parentheses off in this.onInputChange because we don't want the function to be called every time we render
+//the text box but rather when user types something in it.
+//<input type="text" onChange={this.onInputChange}></input>
 
 export default SearchBar;
